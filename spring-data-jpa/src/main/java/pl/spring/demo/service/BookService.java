121995dd6dd6ac87d17d2ff44e8294e75eccc55f
Mikolaj Lech <mikolajj.lech@gmail.com>
2015-07-22 11:50:35 +0200

    Add deleting books.

--- spring-data-jpa/src/main/java/pl/spring/demo/service/BookService.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/service/BookService.java
@@ -6,9 +6,12 @@ import java.util.List;
 
 public interface BookService {
 
-    List<BookTo> findAllBooks();
-    List<BookTo> findBooksByTitle(String title);
-    List<BookTo> findBooksByAuthor(String author);
-
-    BookTo saveBook(BookTo book);
+	List<BookTo> findAllBooks();
+	List<BookTo> findBooksByTitle(String title);
+	List<BookTo> findBooksByAuthor(String author);
+	BookTo findBookById(Long bookId);
+	BookTo saveBook(BookTo book);
+	BookTo getBook(Long bookId);
+	BookTo delete(Long bookId);
+	BookTo addBook(BookTo book);
 }
